37dc97a5680e49304e4bdc55e3351daee26d9b6e
Coding Kaksha <codingkaksha.in@gmail.com>
2025-12-13 22:22:54 +0530

    Update day75.sql

--- solution/day75.sql
+++ solution/day75.sql
@@ -1,1 +1,15 @@
---- solution for video
+/*
+product_id, product_name, price, quanity, sale_date
+
+What is the total revenue generated across all 
+products for the current month?
+*/
+
+select 
+month(sale_date) as months,
+YEAR(sale_date) as years,
+sum(price*quantity) as revenue
+from Products
+where MONTH(sale_date) = MONTH(GETDATE())
+group by month(sale_date),
+YEAR(sale_date)--- solution for video
